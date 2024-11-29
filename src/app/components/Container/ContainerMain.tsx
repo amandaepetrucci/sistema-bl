51b85b53d2426d981f47875a23a37ffa993c4283
Amanda Evangelista <amandae.petrucci@gmail.com>
2024-11-29 15:25:55 -0300

    fix:changes

--- src/app/components/Container/ContainerMain.tsx
+++ src/app/components/Container/ContainerMain.tsx
@@ -2,7 +2,7 @@
 export function ContainerMain(){
   return(
     <div className="mt-5 ml-8">
-      <h2>Bem vindo(a) ao Sistema BL Web - Em desenvolvimento</h2>
+      <h2>Bem vindo(a) ao Sistema BL Web - Em desenvolvimento!</h2>
     </div>
   )
 }
